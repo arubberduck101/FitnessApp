df80518031b7cca1ceb9fd08aa596c0ec3c22d48
Utcassyxz <sunyangxuezhe@gmail.com>
2023-06-11 17:31:04 -0700

    finish login LoginPage

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,29 +1,27 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  const LoginPage({super.key});
 
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        padding: const EdgeInsets.all(8.0),
-        children: <Widget>[
-          const SizedBox(
-            height: 50,
-          ),
-          const Text(
-            "Exercise App",
+        body: ListView(
+      padding: const EdgeInsets.all(8.0),
+      children: <Widget>[
+        const SizedBox(
+          height: 50,
+        ),
+
+        // Text component --> align the text in the center, and make it bold and make font size 30
+        const Text("Exercise App",
             textAlign: TextAlign.center,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-          ),
-          Padding(
-            padding: EdgeInsets.all(16.0),
-            child: LoginForm(),
-          ),
-        ],
-      ),
-    );
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+
+        Padding(padding: EdgeInsets.all(16.0), child: LoginForm()),
+      ],
+    ));
   }
 }
 
@@ -47,53 +45,95 @@ class _LoginFormState extends State<LoginForm> {
     return Form(
       key: _formKey,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
+          // email
           TextFormField(
-            decoration: const InputDecoration(
-              prefixIcon: Icon(Icons.email_outlined),
-              labelText: "Email",
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(100.0),
+              // initialValue: 'Input text',
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.email_outlined),
+                labelText: 'Email',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(100.0),
                   ),
-              )
-              
-            )
-          ),
+                ),
+              ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Please enter your email";
+                }
+                return null;
+              },
+              onSaved: (val) {
+                email = val;
+              }),
 
           SizedBox(
             height: 20,
           ),
           // password
           TextFormField(
-            //intialValue: "input text"
-            decoration: const InputDecoration(
-              
-              labelText: "Password",
-              prefixIcon: Icon(Icons.lock_outline),
-              border: OutlineInputBorder(
+            // initialValue: 'Input text',
+            decoration: InputDecoration(
+              labelText: 'Password',
+              prefixIcon: const Icon(Icons.lock_outline),
+              border: const OutlineInputBorder(
                 borderRadius: BorderRadius.all(
                   Radius.circular(100.0),
-                  ),
+                ),
               ),
-              
             ),
-            validator: (value)  {
-              if(value!.isEmpty){
-                return "Please Enter your email";
-              }
-            }
-          ),
-          
-        ]
-          
-        )
-      );
 
+            obscureText: _obscureText,
+          ),
+
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _obscureText = false;
+                });
+              },
+              child: Text("Show Password")),
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _obscureText = true;
+                });
+              },
+              child: Text("Hide Password")),
+
+SizedBox(
+  height: 40,
+),
+SizedBox(
+  height: 40,
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.center, 
+    children: [
+      ElevatedButton(
+        onPressed: () {
+          setState(() {
+            _obscureText = true;
+          });
+        },
+        child: Text("Sign up"),
+      ),
+      SizedBox(width: 10), 
+      ElevatedButton(
+        onPressed: () {
+          setState(() {
+            _obscureText = true;
+          });
+        },
+        child: Text("Login"),
+      ),
+    ],
+  ),
+),
+
+    ]
+    )
+    );
   }
 }
-
-class SizeBox {
-}
-// are you ok
-// I am soooo hungary 
